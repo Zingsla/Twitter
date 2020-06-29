7a6a3af66fe9cb87df54eabe924732b07ddb07b1
Jacob Franz <franzj@fb.com>
2020-06-29 16:22:48 -0500

    Created User and Tweet data models

---
+++ twitter/Models/User.h
@@ -0,0 +1,22 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by Jacob Franz on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *screenName;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
